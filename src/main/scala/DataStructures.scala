9a9bd0c3d1c78b23b456838fab732ab8ab7156a9
rkoeninger <rkoeninger@att.net>
2014-06-27 13:57:11 -0400

    Finished FPPractice, skipped TR forms

--- src/main/scala/DataStructures.scala
+++ src/main/scala/DataStructures.scala
@@ -1,6 +1,6 @@
 class LRUCache[K, +V](val recentCacheSize: Int) {
   def get(key: K): Option[V] = ???
-  def put(key: K, value: V): Unit = ???
+  def put(key: K, value: Any): Unit = ???
   // O(1) for values in cache
   // O(log n) for get of any key
   // O(log n) for put of any key
